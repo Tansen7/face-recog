f7725013eff2d16b9a1d3ffca0b7e0bdc69fbd30
Tansen7 <80833068+Tansen7@users.noreply.github.com>
2022-04-22 16:10:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,23 @@ classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded()  {
 console.log('Model Loaded!');
 }
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult());
+}
+gotResult(error, results) 
+{
+if (error) {
+    console.error(error);
+
+} else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML =  results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
+
 
 
 
